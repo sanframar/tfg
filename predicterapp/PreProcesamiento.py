6acb21877a3be22db9732e7ccb724e6f53427f41
sanframar <raznnok@gmail.com>
2018-08-13 14:31:50 +0200

    Metodo para eliminar los NaN de nuestro conjunto de datos. No se controla los NaN consecutivos.

--- predicterapp/PreProcesamiento.py
+++ predicterapp/PreProcesamiento.py
@@ -14,3 +14,35 @@ def indicesNulos(dataframe):
 def normalizacionDataframe(dataframe):
     stdsclr = StandardScaler()
     return stdsclr.fit_transform(dataframe)
+
+#Metodo que calcula los nuevos valores para eliminar los NaN de nuestro conjunto de datos
+def calcularValoresNaN(array, indicesNaN):
+    for aux in indicesNaN:
+        if(aux==0):
+            nanEnInicio(array, indicesNaN, aux)
+        if(aux==array.size-1):
+            nanEnFinal(array, indicesNaN, aux)
+        if(aux>0 & aux<array.size-1):
+            nanEnMedio(array, indicesNaN, aux)
+            
+            
+def nanEnInicio(array, indicesNaN, aux):
+    nextValue = array[aux+1]
+    nextValue2 = array[aux+2]
+    newValue = (nextValue + nextValue2)/2
+    print(newValue)
+    array[aux] = newValue
+    
+def nanEnFinal(array, indicesNaN, aux):
+    nextValue = array[aux-1]
+    nextValue2 = array[aux-2]
+    newValue = (nextValue + nextValue2)/2
+    print(newValue)
+    array[aux] = newValue
+
+def nanEnMedio(array, indicesNaN, aux):
+    nextValue = array[aux+1]
+    afterValue = array[aux-1]
+    newValue = (nextValue + afterValue)/2
+    print(newValue)
+    array[aux] = newValue
