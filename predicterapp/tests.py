eed95eefad591fd77dce81516d152bc2fd2af197
sanframar <raznnok@gmail.com>
2019-01-05 23:13:20 +0100

    Tests para comprobar la funcionalidad de la aplicacion. #10

--- predicterapp/tests.py
+++ predicterapp/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
-# Create your tests here.
+from predicterapp.Utils import *
+
+from django.test import TestCase
+
+class UtilsTests(TestCase):
+    
+    def test_diferencia_porcentual_igual(self):
+            """
+            Comprobamos que al ser los dos valores iguales el sistema responde
+            adecuadamente con un cero
+            """
+            valorNuevo = 10
+            valorAnterior = 10
+            self.assertIs(diferenciaPorcentual(valorNuevo, valorAnterior), 0)
+
+    def test_diferencia_porcentual_menor(self):
+            """
+            Comprobamos que al dividir entre cero el sistema muestra
+            como resultado -999999
+            """
+            valorNuevo = 10
+            valorAnterior = 0
+            self.assertEqual(diferenciaPorcentual(valorNuevo, valorAnterior), -999999)
