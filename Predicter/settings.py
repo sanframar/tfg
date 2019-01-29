42571d09300678c42fdf59d084f08d142e0ef5ad
sanframar <raznnok@gmail.com>
2019-01-29 20:48:44 +0100

    Configuracion de archivos estaticos para el deploy.

--- Predicter/settings.py
+++ Predicter/settings.py
@@ -122,5 +122,5 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-STATICFILES_ROOT = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = "/home/kanchi/kanchi.pythonanywhere.com/predicterapp/static"
 
